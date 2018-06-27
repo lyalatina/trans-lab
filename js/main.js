05edb283a5d868ab15b8d3168bfafd097a8df65c
Natalia <nataliamp87@gmail.com>
2018-06-27 19:54:00 -0400

    enlace btn pagina home

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
+
+
 function isNumberKey(evt) {
-  var charCode = (evt.which) ? evt.which : event.keyCode;
-  if (charCode > 31 && (charCode < 48 || charCode > 57))
+ 
+	var charCode = (evt.which) ? evt.which : evt.keyCode;
+	// Added to allow decimal, period, or delete
+	if (charCode == 110 || charCode == 190 || charCode == 46) 
+		return true;
+	
+  if (charCode > 31 && (charCode < 48 || charCode > 57)) 
   
-    return false;
-   
-    
+		return false;
+  
   return true;
-  
-  
+ 
 }
 
-
-
-  
