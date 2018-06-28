8896c2a8e5f0bf98b5226dbc87752095062c41d1
Natalia <nataliamp87@gmail.com>
2018-06-27 20:00:40 -0400

    eslintrc

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
-
-
 function isNumberKey(evt) {
+  var charCode = (evt.which) ? evt.which : evt.keyCode;
  
-	var charCode = (evt.which) ? evt.which : evt.keyCode;
-	// Added to allow decimal, period, or delete
-	if (charCode == 110 || charCode == 190 || charCode == 46) 
-		return true;
+  if (charCode == 110 || charCode == 190 || charCode == 46) 
+    return true;
 	
   if (charCode > 31 && (charCode < 48 || charCode > 57)) 
   
-		return false;
+    return false;
   
   return true;
- 
 }
 
